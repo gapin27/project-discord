0c49c00c74d7b536b6382afb8f142723a881377a
gapin27 <gavin.tannael@gmail.com>
2024-08-16 18:30:56 +0700

    Update meme-dict.py
    Menambahkan looping, menambahkan kamus,menambahkan logika berhenti sebelum program selesai.

--- meme-dict.py
+++ meme-dict.py
@@ -1,9 +1,21 @@
 meme_dict = {
             "CRINGE": "Sesuatu yang sangat aneh atau memalukan",
             "LOL": "Tanggapan umum terhadap sesuatu yang lucu",
+            "ROFL": "tanggapan terhadap lelucon",
+            "SHEESH": "sedikit ketidaksetujuan",
+            "CREEPY": "menakutkan, tidak menyenangkan",
+            "AGGRO": "untuk menjadi agresif/marah"
             }
-word = input("Ketik kata yang tidak Kamu mengerti (gunakan huruf kapital semua!): ")
-if word in meme_dict.keys():
-    print(meme_dict[word])
-else:
-    print("kata tidak di temukan")
+
+
+for i in range(5):
+    word = input("Ketik kata yang tidak Kamu mengerti (gunakan huruf kapital semua!): ")
+
+    if word == "END":
+        print("Terima kasih telah menggunakan kamus ini")
+        break
+    
+    if word in meme_dict.keys():
+        print(meme_dict[word])
+    else:
+        print("kata tidak di temukan")
